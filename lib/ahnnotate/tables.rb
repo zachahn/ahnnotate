a4175d5ef0bd338d28a6636a65ff236bd724145d
Zach Ahn <engineering@zachahn.com>
2019-10-16 23:14:51 -0400

    Hide warnings when Rails can't resolve column type
    Rails treats `time with time zone` as a string since there's no Ruby
    equivalent of that class. It prints a warning message.
    
    Since Ahnnotate is able to handle this column type, it doesn't need to
    print a warning here.

--- lib/ahnnotate/tables.rb
+++ lib/ahnnotate/tables.rb
@@ -17,59 +17,66 @@ module Ahnnotate
       end
 
       @connection.public_send(data_sources_method_name).each do |table_name|
-        primary_key = ActiveRecord::Base.get_primary_key(table_name)
+        begin
+          original_stderr = $stderr
+          $stderr = StringIO.new
 
-        columns = @connection.columns(table_name).map do |c|
-          is_primary_key =
-            if primary_key.is_a?(Array)
-              primary_key.include?(c.name)
-            else
-              primary_key == c.name
-            end
+          primary_key = ActiveRecord::Base.get_primary_key(table_name)
 
-          Column.new(
-            name: c.name,
-            sql_type: c.sql_type,
-            nullable: c.null,
-            primary_key: is_primary_key,
-            default: c.default
-          )
-        end
+          columns = @connection.columns(table_name).map do |c|
+            is_primary_key =
+              if primary_key.is_a?(Array)
+                primary_key.include?(c.name)
+              else
+                primary_key == c.name
+              end
 
-        indexes = @connection.indexes(table_name).map do |i|
-          comment =
-            if i.respond_to?(:comment)
-              i.comment
-            end
-
-          Index.new(
-            name: i.name,
-            columns: i.columns,
-            unique: i.unique,
-            comment: comment
-          )
-        end
-
-        foreign_keys =
-          if @abilities.foreign_key?
-            @connection.foreign_keys(table_name).map do |fk|
-              ForeignKey.new(
-                name: fk.name,
-                from_column: fk.column,
-                to_table: fk.to_table,
-                to_column: fk.primary_key
-              )
-            end
-          else
-            []
+            Column.new(
+              name: c.name,
+              sql_type: c.sql_type,
+              nullable: c.null,
+              primary_key: is_primary_key,
+              default: c.default
+            )
           end
 
-        yield Table.new(
-          name: table_name,
-          columns: columns,
-          indexes: indexes,
-          foreign_keys: foreign_keys,
-        )
+          indexes = @connection.indexes(table_name).map do |i|
+            comment =
+              if i.respond_to?(:comment)
+                i.comment
+              end
+
+            Index.new(
+              name: i.name,
+              columns: i.columns,
+              unique: i.unique,
+              comment: comment
+            )
+          end
+
+          foreign_keys =
+            if @abilities.foreign_key?
+              @connection.foreign_keys(table_name).map do |fk|
+                ForeignKey.new(
+                  name: fk.name,
+                  from_column: fk.column,
+                  to_table: fk.to_table,
+                  to_column: fk.primary_key
+                )
+              end
+            else
+              []
+            end
+
+          yield Table.new(
+            name: table_name,
+            columns: columns,
+            indexes: indexes,
+            foreign_keys: foreign_keys,
+          )
+        ensure
+          $stderr = original_stderr
+        end
       end
     end
 
